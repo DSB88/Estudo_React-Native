3272e8360f3ffd715a7c8a807f105a1323639538
DSB88 <diego.brandao@fatec.sp.gov.br>
2022-06-07 14:51:36 -0300

    Aula 28

--- src/components/Titulo.js
+++ src/components/Titulo.js
@@ -1,2 +1,13 @@
 import React from 'react'
-import { }
+import { View, Text } from 'react-native'
+import estilo from './estilo'
+import Estilo from './estilo'
+
+export default props => (
+    // <View>
+<React.Fragment>
+        <Text style={Estilo.txtG}> {props.principal}</Text>
+        <Text>{props.secundario}</Text>
+    {/* // </View> */}
+</React.Fragment>
+)
