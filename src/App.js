89c9930cd336c1c8394948df6b4b42d3cad41a6c
DSB88 <diego.brandao@fatec.sp.gov.br>
2022-06-20 10:51:03 -0300

    Aula 33

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
-import splitLayoutProps from 'react-native/Libraries/StyleSheet/splitLayoutProps'
-
-// import Aleatorio from './components/Aleatorio'
-//  import MinMax from './components/MinAndMax'
-// import CompPadrao, { Comp1, Comp2 } from './components/Multi'
-// import Primeiro from './components/Primeiro'
-// import Titulo from './components/Titulo'
 import Botao from './components/Botao'
-import Contador from './components/Contador'
-
+import Pai from './components/direta/Pai'
 
 export default () => (
     <View style={style.App}>
 
-        <Contador inicial={100} passo={13} />
-        <Contador />
-
+    <Pai>       </Pai>
 
     </View>
 )
