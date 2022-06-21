6fd4d219b218346bbe79bceaaccec7c12a378a40
DSB88 <diego.brandao@fatec.sp.gov.br>
2022-06-21 20:49:01 -0300

    Aula 46

---
+++ src/components/produtos/ListaProdutosV2.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Text, FlatList } from 'react-native'
+import estilo from '../estilo'
+import Estilo from '../estilo'
+import produtos from './produtos'
+
+export default props => {
+
+    return (
+        <>
+            <Text style={Estilo.txtG}>
+                Lista de Produtos
+            </Text>
+            <FlatList
+                data={produtos}
+                keyExtractor={i => i.id}
+                renderItem={
+                    ({ item: q }) => {
+                        return <Text style={estilo.txtP}>{q.id}) Produto: {q.nome} Preço: R${q.preco} </Text>
+                    }
+                }
+            />
+        </>
+    )
+}
