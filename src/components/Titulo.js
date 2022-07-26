5d4bbe16b0483a1db24899b72e02f8c61149f898
DSB88 <diego.brandao@fatec.sp.gov.br>
2022-07-26 13:05:12 -0300

    fix Corrigindo projeto

--- src/components/Titulo.js
+++ src/components/Titulo.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import estilo from './estilo'
-import Estilo from './estilo'
+import React from "react";
+import { View, Text } from "react-native";
+import Estilo from "./estilo";
 
-export default props => (
-    // <View>
-<React.Fragment>
-        <Text style={Estilo.txtG}> {props.principal}</Text>
-        <Text>{props.secundario}</Text>
+export default (props) => (
+  // <View>
+  <React.Fragment>
+    <Text style={Estilo.txtG}> {props.principal}</Text>
+    <Text>{props.secundario}</Text>
     {/* // </View> */}
-</React.Fragment>
-)
+  </React.Fragment>
+);
